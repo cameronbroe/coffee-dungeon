a91f0c3658b3e1fb2536c08d876b2d58fcf09add
Cameron Roe <cameron@cameronbroe.com>
2025-08-05 00:09:09 -0500

    feat: draw player sprite

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,12 @@
 #include "bn_regular_bg_ptr.h"
 
 #include "bn_regular_bg_items_test_room.h"
+#include "bn_sprite_items_player.h"
+#include "bn_sprite_ptr.h"
 
 void main_scene() {
+    bn::sprite_ptr player_sprite = bn::sprite_items::player.create_sprite(0, 0);
+
     while (true) {
         bn::core::update();
     }
