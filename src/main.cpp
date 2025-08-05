4446916a3549e7621dde7f164ac617cda78fa04d
Cameron Roe <cameron@cameronbroe.com>
2025-08-05 01:23:29 -0500

    feat: move camera around map with player fixed at origin

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,27 @@
+#include "bn_camera_ptr.h"
 #include "bn_core.h"
+#include "bn_keypad.h"
 #include "bn_regular_bg_ptr.h"
 
 #include "bn_regular_bg_items_test_room.h"
 #include "bn_sprite_items_player.h"
 #include "bn_sprite_ptr.h"
 
-void main_scene() {
+void main_scene(bn::camera_ptr& camera) {
     bn::sprite_ptr player_sprite = bn::sprite_items::player.create_sprite(0, 0);
 
     while (true) {
+        if (bn::keypad::left_held()) {
+            camera.set_x(camera.x() - 1);
+        } else if (bn::keypad::right_held()) {
+            camera.set_x(camera.x() + 1);
+        }
+
+        if (bn::keypad::up_held()) {
+            camera.set_y(camera.y() - 1);
+        } else if (bn::keypad::down_held()) {
+            camera.set_y(camera.y() + 1);
+        }
         bn::core::update();
     }
 }
@@ -18,7 +31,10 @@ int main() {
 
     bn::regular_bg_ptr background = bn::regular_bg_items::test_room.create_bg(0, 0);
 
+    bn::camera_ptr camera = bn::camera_ptr::create(0, 0);
+    background.set_camera(camera);
+
     while (true) {
-        main_scene();
+        main_scene(camera);
     }
 }
